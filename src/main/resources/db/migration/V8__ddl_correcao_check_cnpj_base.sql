4ea6316b41f24c3e7c2961c7baac7aa197c16e47
Edwi Feitoza <edwi.slacker@gmail.com>
2020-04-12 15:15:59 -0300

    Scripts SQL para popular as 5 tabelas, com 5k registros em cada

---
+++ src/main/resources/db/migration/V8__ddl_correcao_check_cnpj_base.sql
@@ -0,0 +1,7 @@
+ALTER TABLE solicitacao DROP CONSTRAINT identificador_participante_administrado_valido;
+ALTER TABLE solicitacao DROP CONSTRAINT identificador_if_proponente_valido;
+ALTER TABLE solicitacao ADD CONSTRAINT identificador_participante_administrado_valido CHECK (identificador_participante_administrado >= 1000000 AND identificador_participante_administrado <= 99999999);
+ALTER TABLE solicitacao ADD CONSTRAINT identificador_if_proponente_valido CHECK (identificador_if_proponente >= 1000000 AND identificador_if_proponente <= 99999999);
+
+ALTER TABLE identificador_contrato DROP CONSTRAINT cnpj_base_if_credora_original_valido;
+ALTER TABLE identificador_contrato ADD CONSTRAINT cnpj_base_if_credora_original_valido CHECK (cnpj_base_if_credora_original >= 1000000 AND cnpj_base_if_credora_original <= 99999999);
