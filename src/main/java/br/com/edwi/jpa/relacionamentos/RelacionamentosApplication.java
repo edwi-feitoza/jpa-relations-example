fef7efd5eb0cb3a880a34c45af48515cc400fada
Edwi Feitoza <edwi.slacker@gmail.com>
2020-08-17 01:49:56 -0300

    Implementação de chamada e captura de resultados da Stored Procedure da forma mais simples possível de fazer e manter

--- src/main/java/br/com/edwi/jpa/relacionamentos/RelacionamentosApplication.java
+++ src/main/java/br/com/edwi/jpa/relacionamentos/RelacionamentosApplication.java
@@ -1,14 +1,47 @@
 package br.com.edwi.jpa.relacionamentos;
 
+import br.com.edwi.jpa.relacionamentos.entities.ClienteEntity;
+import br.com.edwi.jpa.relacionamentos.enums.TipoPessoaEnum;
+import br.com.edwi.jpa.relacionamentos.repositories.ClienteRepository;
+import lombok.AllArgsConstructor;
+import lombok.RequiredArgsConstructor;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
+import java.util.Collection;
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
+
 @SpringBootApplication
 @EnableTransactionManagement
-public class RelacionamentosApplication {
+@RequiredArgsConstructor
+public class RelacionamentosApplication implements CommandLineRunner {
+
+	private final ClienteRepository clienteRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(RelacionamentosApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+
+		Optional<Collection<Object[]>> seiLah = this.clienteRepository.testStoredProcedure("68228758162", "F");
+		List<ClienteEntity> clientesFromStoredProcedure = seiLah.orElseThrow(RuntimeException::new).stream().map(result -> new ClienteEntity(
+				(Integer) result[0],
+				null,
+				null,
+				TipoPessoaEnum.valueOf(((Character) result[1]).toString()),
+				null,
+				(String) result[2],
+				(String) result[3],
+				null,
+				null,
+				null)
+		).collect(Collectors.toList());
+		System.out.println(clientesFromStoredProcedure);
 	}
 }
--- src/main/java/br/com/edwi/jpa/relacionamentos/repositories/ClienteRepository.java
+++ src/main/java/br/com/edwi/jpa/relacionamentos/repositories/ClienteRepository.java
@@ -5,10 +5,15 @@ import br.com.edwi.jpa.relacionamentos.entities.SolicitacaoKey;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
+
+import java.util.Collection;
 import java.util.Optional;
 
 public interface ClienteRepository extends JpaRepository<ClienteEntity, SolicitacaoKey> {
 
     @Query("SELECT c FROM ClienteEntity c WHERE c.id = :id")
     Optional<ClienteEntity> findById(@Param("id") Integer id);
+
+    @Query(value = "EXEC getClientes :cnpjCpf, :tipo", nativeQuery = true)
+    Optional<Collection<Object[]>> testStoredProcedure(String cnpjCpf, String tipo);
 }
